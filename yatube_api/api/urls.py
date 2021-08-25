5e95fed9987b52ee081f11fef0594a3c5deb179d
WeiGO <nimfeya23@yandex.ru>
2021-08-25 10:10:57 +0300

    sprint_10_review_1_fixJWT

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
+
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'posts', PostViewSet)
 router_v1.register(r'groups', GroupViewSet)
@@ -13,8 +14,6 @@ router_v1.register(r'follow', FollowViewSet, basename='follow')
 
 
 urlpatterns = [
-    path('v1/jwt/create/', TokenObtainPairView),
-    path('v1/jwt/refresh/', TokenRefreshView),
-    path('v1/jwt/verify/', TokenVerifyView),
     path('v1/', include(router_v1.urls)),
+    path('v1/jwt/', include('api.inner', namespace='api')),
 ]
